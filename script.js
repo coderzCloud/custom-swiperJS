7efbf40a448700e55d250711298be4af12750a58
RZcodeCloud <zerjarenald@gmail.com>
2023-11-24 15:10:00 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,43 @@
+console.log("Its conected");
+
+let photoSwiper = new Swiper(".swiper.is-card", {
+  effect: "cards",
+  grabCursor: true,
+  loop: true,
+  keyboard: true,
+});
+
+$(".slider-faq_component").each(function (index) {
+  const swiper = new Swiper($(this).find(".swiper")[0], {
+    slidesPerView: 1,
+    keyboard: true,
+    mousewheel: {
+      forceToAxis: true,
+    },
+    slideToClickedSlide: true,
+    followFinger: true,
+    spaceBetween: "5%",
+    breakpoints: {
+      // mobile landscape
+      480: {
+        slidesPerView: 1,
+        spaceBetween: "4%",
+      },
+      // tablet
+      768: {
+        slidesPerView: 2,
+        spaceBetween: "4%",
+      },
+      // desktop
+      992: {
+        slidesPerView: 3,
+        spaceBetween: "2%",
+      },
+    },
+    navigation: {
+      nextEl: $(this).find(".swiper-next")[0],
+      prevEl: $(this).find(".swiper-prev")[0],
+      disabledClass: "is-disabled",
+    },
+  });
+});
